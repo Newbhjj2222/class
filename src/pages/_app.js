a3f675feb262848f78b9906516962ac7cdf38904
NewtalentsG <newtalents403@gmail.com>
2025-11-14 16:16:42 +0200

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,7 @@
 import "@/styles/globals.css";
+import { pdfjs } from 'react-pdf';
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
