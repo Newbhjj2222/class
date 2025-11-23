c514718759eb3a5e1a77b1860b83ebf7b9f98257
NewtalentsG <newtalents403@gmail.com>
2025-11-23 05:51:27 +0200

    Create read.js

---
+++ src/pages/read.js
@@ -0,0 +1,39 @@
+// pages/read.js
+import { db } from "@/components/firebase";
+import { doc, getDoc } from "firebase/firestore";
+
+export async function getServerSideProps(context) {
+  const { id } = context.query;
+
+  const ref = doc(db, "books", id);
+  const snapshot = await getDoc(ref);
+
+  if (!snapshot.exists()) {
+    return { notFound: true };
+  }
+
+  return {
+    props: {
+      book: snapshot.data()
+    }
+  };
+}
+
+export default function ReadBook({ book }) {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>{book.title}</h1>
+      <p>By: {book.author}</p>
+
+      <iframe
+        src={book.url}
+        style={{
+          width: "100%",
+          height: "90vh",
+          border: "none",
+          marginTop: "10px"
+        }}
+      ></iframe>
+    </div>
+  );
+}
