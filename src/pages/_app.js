8845fd36c19594e0c7c57b7172668cf0612375f1
NewtalentsG <newtalents403@gmail.com>
2025-12-23 19:47:26 +0200

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,20 @@
 import "@/styles/globals.css";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div className="appLayout">
+      {/* Sidebar (Header) */}
+      <Header />
+
+      {/* Main content */}
+      <main className="mainContent">
+        <Component {...pageProps} />
+      </main>
+
+      {/* Footer kuri desktop izajya hejuru ya sidebar */}
+      <Footer />
+    </div>
+  );
 }
